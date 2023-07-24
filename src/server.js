127a4eca5ceb8b70c6f96cfad0ed584e743fdf59
kes <allinpositive@gmail.com>
2023-07-24 12:01:48 +0900

    소켓 메시지

--- src/server.js
+++ src/server.js
@@ -1,18 +1,40 @@
-import express from "express";
-import http from "http";
-import WebSocket from "ws";
-import path from 'path';
+import path, { parse } from 'path';
 const __dirname = path.resolve();
-const app = express();
 
+
+import http from "http";
+import WebSocket, { WebSocketServer } from "ws";
+import express from "express";
+const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/src/views");
 app.use("/public", express.static(__dirname + "/src/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-app.listen(3000, function() {
-    console.log("server is running")
-})
+const wss = new WebSocketServer({ server })
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  console.log("Connected to Browser ✅");
+  socket["nickname"] = "Anon";
+  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("message", (msg) => {
+    msg = msg.toString('utf-8')
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
+  });
+});
+
+server.listen(3000, handleListen);
